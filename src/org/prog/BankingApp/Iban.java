b4f43ca5ebeabaf1ea5da9313768d871ec526ee1
RobertLutscht <Jonas.Unruh@t-online.de>
2020-06-18 16:27:29 +0200

    IBANcheck hinzugefügt

--- src/org/prog/BankingApp/Iban.java
+++ src/org/prog/BankingApp/Iban.java
@@ -1,4 +1,9 @@
 package org.prog.BankingApp;
 
 public class Iban {
+    private boolean check;
+
+    public boolean getCheck() {
+        return false;
+    }
 }
--- src/org/prog/BankingApp/Numbers.java
+++ src/org/prog/BankingApp/Numbers.java
@@ -1,4 +1,47 @@
 package org.prog.BankingApp;
 
-public class numbers {
+import java.math.BigInteger;
+
+public class Numbers {
+
+    public static boolean ibancheck(String iban) {
+
+
+        boolean check = false;
+
+        iban = iban.replaceAll(" ", "");
+
+        if (iban.length() > 24) {
+            check = false;
+
+        }
+        String laenderkennung = iban.substring(0, 1);
+        String pruefziffer = iban.substring(2, 4);
+        String bankleitzahl = iban.substring(4, 11);
+        String kontonummer = iban.substring(12, iban.length());
+        String langezahl = iban.substring(4, 22);
+
+
+        String plus = "131400";
+        String langlangezahl = langezahl + plus;
+
+        BigInteger ziffer = new BigInteger(pruefziffer);
+
+
+        BigInteger valid = new BigInteger(langlangezahl);
+
+        BigInteger pruefen = valid.mod(new BigInteger("97"));
+        String u = "98";
+        BigInteger j = new BigInteger(u);
+        BigInteger l = j.subtract(pruefen);
+        //= l == ziffer;
+
+        if (l.equals(ziffer)) {
+            check = true;
+        }
+        return check;
+
+
+    }
 }
+
