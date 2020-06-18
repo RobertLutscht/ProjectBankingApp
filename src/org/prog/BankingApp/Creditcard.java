334ec092bfa66ed8e7ddf514940513fae53f0163
RobertLutscht <Jonas.Unruh@t-online.de>
2020-06-18 22:12:36 +0200

    update

--- src/org/prog/BankingApp/Creditcard.java
+++ src/org/prog/BankingApp/Creditcard.java
@@ -6,11 +6,15 @@ public class Creditcard extends Account {
     private int limit;
 
     //Konstruktor
-    public Creditcard(String owner, int limit){
+    public Creditcard(String owner, int limit, String bic,String iban){
         this.owner = owner;
         this.limit = limit;
-        this.bic =;
-        this.iban =;
+        this.bic = bic;
+        if(Numbers.ibancheck(iban)){
+            this.iban = iban;
+        } else {
+            System.out.println("Sie haben eine falsche IBAN eingegeben, versuchen Sie es erneut.");
+        }
     }
 
     //Setter für das Limit
