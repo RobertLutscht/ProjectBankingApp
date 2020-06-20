596157fd9680ce9165709f603f81e3e1fe47cf55
P0907 <66681734+P0907@users.noreply.github.com>
2020-06-20 15:31:13 +0200

    Update User.java
    Added private Variables and Konstruktur

--- src/org/prog/BankingApp/User.java
+++ src/org/prog/BankingApp/User.java
@@ -2,14 +2,34 @@ package org.prog.BankingApp;
 
 public class User {
 
-    private String name;
-    private final int USERID;
+    private String firstName;
+    private String lastName;
+    private int birthday;
+    private String street;
+    private int plz;
+    private String city;
+    private String country;
+    private String phoneNumber;
+    private String  eMail;
+    private final int userID;
+    private String password;
 
 
-    public User(String name, int userid){
-        this.name = name;
-        this.USERID = userid;
+    public User(int userID, String password, String firstName, String lastName, int birthday, String street, int plz, String city, String country, String phoneNumber, String eMail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.street = street;
+        this.plz = plz;
+        this.city = city;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+        this.eMail = eMail;
+        this.userID = userID;
+        this.password = password;
     }
+
+
 
 
 }
