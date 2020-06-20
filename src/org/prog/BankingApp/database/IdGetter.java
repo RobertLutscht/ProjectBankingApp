fb11e37429b4babe3d73a70d792241d5569e4eaa
heggiG <florianheck0@gmail.com>
2020-06-20 16:35:44 +0200

    new util class für jonas zum implementen

---
+++ src/org/prog/BankingApp/database/IdGetter.java
@@ -0,0 +1,23 @@
+package org.prog.BankingApp.database;
+
+public class IdGetter {
+
+    private IdGetter() {
+        //utility class
+    }
+
+    public static int getNextAccId() {
+        //TODO @jonas hier die id aus der db getten
+        return 0;
+    }
+
+    public static int getNextOwnerId() {
+        //TODO @jonas hier die ownerid aus der db getten
+        return 0;
+    }
+
+    public static int getNextTracId() {
+        //TODO
+        return 0;
+    }
+}
